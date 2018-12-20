b6a2ed34b6855c612a6a3bd3c34f2035dd21cbbd
Leo Melin <leo.melin@gofore.com>
2018-12-20 14:48:38 +0200

    Set the postgres version for compatibility

--- scripts/database/start_dev_database.sh
+++ scripts/database/start_dev_database.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run --rm -ePOSTGRES_DB=wds_db -e POSTGRES_USER=wds -e POSTGRES_PASSWORD=password -p 45433:5432 postgres:latest
-
+docker run --rm -ePOSTGRES_DB=wds_db -e POSTGRES_USER=wds -e POSTGRES_PASSWORD=password -p 45433:5432 postgres:10.6
