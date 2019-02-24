9843b0e236029755ed2c133719adfd6e00aca497
Leo Melin <leo.melin@gofore.com>
2019-02-24 21:05:36 +0200

    Some more refactoring for ansible deploy + added support for deploying only selected tags

--- packages/infra/ansible/entrypoint.sh
+++ packages/infra/ansible/entrypoint.sh
@@ -3,9 +3,13 @@
 eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/id_rsa
 
+if [ ! -z "$2" ]; then
+  TAG_FILTER=--tags\ "$2"
+fi
+
 if [ "$1" == "quick" ]; then
   # Quick deploy, just deploy the app, no firewall, certbot, docker and hardening stuff
-  env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i hosts playbook.yml --skip-tags "firewall,certbot,docker-ce,hardening"
+  env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i hosts playbook.yml --skip-tags "firewall,certbot,docker-ce,hardening" $TAG_FILTER
   exit 0
 fi
 
