063d6e317dea88770f3775d82b6920e3bf97ecf5
Leo Melin <leo.melin@gofore.com>
2019-02-24 11:26:11 +0200

    Removed explicit call to dotenv.configure as it seems to be called automatically

--- packages/wds-client/src/index.tsx
+++ packages/wds-client/src/index.tsx
@@ -1,13 +1,10 @@
 import 'react-app-polyfill/ie11'
-import dotenv from 'dotenv'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
-
-dotenv.config()
 
 ReactDOM.render(
   <BrowserRouter>
