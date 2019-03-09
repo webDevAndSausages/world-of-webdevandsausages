0d73409ac94d9f8d6eedcd6f89285a7c0f98ef4f
Leo Melin <leo.melin@gofore.com>
2019-03-09 17:30:52 +0200

    Implementeded user listing with jinja2 template

--- packages/infra/ansible/admin-entrypoint.sh
+++ packages/infra/ansible/admin-entrypoint.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/id_rsa
 
 ansible-galaxy install andrewvaughan.prompt
-env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i hosts admin-playbook.yml --tags "$1"
+env ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_STDOUT_CALLBACK=debug ansible-playbook -i hosts admin-playbook.yml --tags "$1"
