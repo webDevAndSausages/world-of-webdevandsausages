c77691f8c25f83ac7dc0e514c49b45ad78de484d
Leo Melin <leo.melin@gofore.com>
2019-03-18 20:48:46 +0200

    Fiddled around with validation logic

--- packages/wds-event-tool/src/main/kotlin/org/webdevandsausages/events/controller/PatchEvent.kt
+++ packages/wds-event-tool/src/main/kotlin/org/webdevandsausages/events/controller/PatchEvent.kt
@@ -1,43 +1,38 @@
 package org.webdevandsausages.events.controller
 
 import arrow.core.Either
-import arrow.core.Some
 import org.http4k.contract.ContractRoute
 import org.http4k.contract.div
 import org.http4k.contract.meta
 import org.http4k.core.*
+import org.http4k.lens.string
 import org.webdevandsausages.events.Router
 import org.webdevandsausages.events.dto.EventUpdateInDto
-import org.webdevandsausages.events.dto.whenValid
 import org.webdevandsausages.events.error.toResponse
 import org.webdevandsausages.events.service.GetEventByIdService
 import org.webdevandsausages.events.service.UpdateEventService
-import org.webdevandsausages.events.utils.WDSJackson.auto
 
 object PatchEvent {
-    private val eventUpdateRequestLens = Body.auto<EventUpdateInDto>().toLens()
+    private val eventUpdateRequestLens =
+        Body.string(ContentType.APPLICATION_JSON).map { json -> EventUpdateInDto.from(json) }.toLens()
 
     fun route(updateEvent: UpdateEventService, findByIdService: GetEventByIdService): ContractRoute {
-        fun handlepatchEvent(eventId: Long): HttpHandler = { req: Request ->
+        fun handlepatchEvent(eventId: Long): HttpHandler = lambda@{ req: Request ->
             /* TODO: When updating maxParticipants, this ticket
-               https://github.com/webDevAndSausages/world-of-webdevandsausages/issues/35 needs to be considered */
-
+                   https://github.com/webDevAndSausages/world-of-webdevandsausages/issues/35 needs to be considered */
             val oldEvent = findByIdService(eventId)
-            when (oldEvent) {
-                is Either.Left -> oldEvent.a.toResponse()
-                is Either.Right -> {
-                    val eventUpdateInDto = eventUpdateRequestLens(req)
-                    eventUpdateInDto whenValid {
-                        updateEvent(eventId, eventUpdateInDto).let { reg ->
-                            when (reg) {
-                                is Either.Left -> reg.a.toResponse()
-                                is Either.Right -> Router.eventResponseLens(
-                                    reg.b,
-                                    Response(Status.OK)
-                                )
-                            }
-                        }
-                    }
+            if (oldEvent is Either.Left) return@lambda oldEvent.a.toResponse()
+            val eitherValidatedOrError = eventUpdateRequestLens(req)
+            if (eitherValidatedOrError is Either.Left) return@lambda eitherValidatedOrError.a.toResponse()
+
+            val validatedEventUpdateInDto = (eitherValidatedOrError as Either.Right).b
+            updateEvent(eventId, validatedEventUpdateInDto).let { reg ->
+                when (reg) {
+                    is Either.Left -> reg.a.toResponse()
+                    is Either.Right -> Router.eventResponseLens(
+                        reg.b,
+                        Response(Status.OK)
+                    )
                 }
             }
         }
--- packages/wds-event-tool/src/main/kotlin/org/webdevandsausages/events/dto/EventUpdateInDto.kt
+++ packages/wds-event-tool/src/main/kotlin/org/webdevandsausages/events/dto/EventUpdateInDto.kt
@@ -1,6 +1,8 @@
 package org.webdevandsausages.events.dto
 
+import arrow.core.Either
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.base.CaseFormat
 import com.markodevcic.kvalidation.ValidatorBase
 import com.markodevcic.kvalidation.onError
@@ -35,7 +37,88 @@ data class EventUpdateInDto(
     val registrationOpens: Timestamp? = null,
     val volume: Int? = null,
     val sponsorLink: String? = null
-)
+) {
+    companion object {
+        fun from(json: String): Either<EventError.ValidationError, EventUpdateInDto> {
+            try {
+                class EventUpdateInDtoValidator(consumer: EventUpdateInDto) : ValidatorBase<EventUpdateInDto>(consumer)
+                val dto = ObjectMapper().readValue(json, EventUpdateInDto::class.java)
+                val validator = EventUpdateInDtoValidator(dto)
+
+                with(dto) {
+                    mapOf(
+                        maxParticipants to {
+                            validator.forProperty { maxParticipants } rules {
+                                gt(0)
+                            } onError {
+                                errorMessage("Max participants must be greater than 0")
+                            }
+                        },
+                        name to {
+                            validator.forProperty { name } rules {
+                                length(255)
+                            } onError {
+                                errorMessage("Name max length is 255 chars")
+                            }
+                        },
+                        contact to {
+                            validator.forProperty { contact } rules {
+                                length(255)
+                            } onError {
+                                errorMessage("Contact max length is 255 chars")
+                            }
+                        },
+                        date to {
+                            validator.forProperty { date } rules {
+                                mustBe { it!!.after(Timestamp.valueOf(LocalDateTime.now())) }
+                            } onError {
+                                errorMessage("Date must be in the future")
+                            }
+                        },
+                        details to {
+                            validator.forProperty { details } rules {
+                                length(1024)
+                            } onError {
+                                errorMessage("Details max length is 1024 chars")
+                            }
+                        },
+                        location to {
+                            validator.forProperty { location } rules {
+                                length(255)
+                            } onError {
+                                errorMessage("Location max length is 255 chars")
+                            }
+                        },
+                        registrationOpens to {
+                            validator.forProperty { registrationOpens } rules {
+                                mustBe { it!!.after(Timestamp.valueOf(LocalDateTime.now())) }
+                            } onError {
+                                errorMessage("Registration opens must be in the future")
+                            }
+                        },
+                        sponsorLink to {
+                            validator.forProperty { sponsorLink } rules {
+                                length(255)
+                            } onError {
+                                errorMessage("SponsorLink max length is 255 chars")
+                            }
+                        }
+                    ).filter { it.key != null }
+                        .forEach { it.value() } // Filter out null values and execute validators for non-nulls
+
+                    val validationResult = validator.validate()
+
+                    return Either.cond(validationResult.isValid,
+                        { this },
+                        { EventError.ValidationError(validationResult.validationErrors.map { it.message }.joinToString("\n")) }
+                    )
+                }
+            } catch (e: Exception) {
+                return Either.left(EventError.ValidationError("Invalid JSON input"))
+            }
+        }
+    }
+}
 
 fun EventUpdateInDto.toEventUpdates(): EventUpdates =
     this.javaClass.kotlin.declaredMemberProperties.fold(
@@ -50,76 +133,3 @@ fun EventUpdateInDto.toEventUpdates(): EventUpdates =
             else
                 acc
         })
-
-
-class EventUpdateInDtoValidator(consumer: EventUpdateInDto) : ValidatorBase<EventUpdateInDto>(consumer) {}
-
-infix fun EventUpdateInDto.whenValid(task: () -> Response): Response {
-    val validator = EventUpdateInDtoValidator(this)
-
-    mapOf(
-        maxParticipants to {
-            validator.forProperty { maxParticipants } rules {
-                gt(0)
-            } onError {
-                errorMessage("Max participants must be greater than 0")
-            }
-        },
-        name to {
-            validator.forProperty { name } rules {
-                length(255)
-            } onError {
-                errorMessage("Name max length is 255 chars")
-            }
-        },
-        contact to {
-            validator.forProperty { contact } rules {
-                length(255)
-            } onError {
-                errorMessage("Contact max length is 255 chars")
-            }
-        },
-        date to {
-            validator.forProperty { date } rules {
-                mustBe { it!!.after(Timestamp.valueOf(LocalDateTime.now())) }
-            } onError {
-                errorMessage("Date must be in the future")
-            }
-        },
-        details to {
-            validator.forProperty { details } rules {
-                length(1024)
-            } onError {
-                errorMessage("Details max length is 1024 chars")
-            }
-        },
-        location to {
-            validator.forProperty { location } rules {
-                length(255)
-            } onError {
-                errorMessage("Location max length is 255 chars")
-            }
-        },
-        registrationOpens to {
-            validator.forProperty { registrationOpens } rules {
-                mustBe { it!!.after(Timestamp.valueOf(LocalDateTime.now())) }
-            } onError {
-                errorMessage("Registration opens must be in the future")
-            }
-        },
-        sponsorLink to {
-            validator.forProperty { sponsorLink } rules {
-                length(255)
-            } onError {
-                errorMessage("SponsorLink max length is 255 chars")
-            }
-        }
-    ).filter { it.key != null }.forEach { it.value() } // Filter out null values and execute validators for non-nulls
-
-    val validationResult = validator.validate()
-
-    return when (validationResult.isValid) {
-        true -> task()
-        false -> EventError.ValidationError(validationResult.validationErrors.map { it.message }.joinToString("\n")).toResponse()
-    }
-}
