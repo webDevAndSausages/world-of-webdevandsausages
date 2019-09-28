606c14e405b9d35b004d4cebaabf2a20c4a382b3
Richard Van Camp <richard.vancamp@gofore.com>
2019-09-28 11:40:24 +0300

    fix (wds-site): prevent purging of tailwind selectors from prod build

--- packages/wds-site-v4/rollup.config.js
+++ packages/wds-site-v4/rollup.config.js
@@ -4,20 +4,22 @@ import commonjs from 'rollup-plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
 import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
-import { terser } from 'rollup-plugin-terser'
+import {terser} from 'rollup-plugin-terser'
 import config from 'sapper/config/rollup.js'
 import pkg from './package.json'
 import getPreprocessor from 'svelte-preprocess'
 import postcss from 'rollup-plugin-postcss'
-import PurgeSvelte from 'purgecss-from-svelte'
 import path from 'path'
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning)
-const dedupe = (importee) => importee === 'svelte' || importee.startsWith('svelte/')
+	(warning.code === 'CIRCULAR_DEPENDENCY' &&
+		/[/\\]@sapper[/\\]/.test(warning.message)) ||
+	onwarn(warning)
+const dedupe = importee =>
+	importee === 'svelte' || importee.startsWith('svelte/')
 
 const postcssPlugins = (purgecss = false) => {
 	return [
@@ -28,29 +30,19 @@ const postcssPlugins = (purgecss = false) => {
 		// Do not purge the CSS in dev mode to be able to play with classes in the browser dev-tools.
 		purgecss &&
 			require('@fullhuman/postcss-purgecss')({
-				content: [ './**/*.svelte', './src/template.html' ],
-				extractors: [
-					{
-						extractor: PurgeSvelte,
-
-						// Specify the file extensions to include when scanning for
-						// class names.
-						extensions: [ 'svelte', 'html' ]
-					}
-				],
-				// Whitelist selectors to stop Purgecss from removing them from your CSS.
-				whitelist: []
+				content: ['./**/*.html', './**/*.svelte'],
+				defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 			}),
-		!dev && require('cssnano')
+		!dev && require('cssnano'),
 	].filter(Boolean)
 }
 
 const preprocess = getPreprocessor({
 	transformers: {
 		postcss: {
-			plugins: postcssPlugins() // Don't need purgecss because Svelte handle unused css for you.
-		}
-	}
+			plugins: postcssPlugins(), // Don't need purgecss because Svelte handle unused css for you.
+		},
+	},
 })
 
 export default {
@@ -61,50 +53,50 @@ export default {
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode),
-				ENVIRONMENT: JSON.stringify(mode)
+				ENVIRONMENT: JSON.stringify(mode),
 			}),
 			svelte({
 				dev,
 				hydratable: true,
 				emitCss: true,
-				preprocess
+				preprocess,
 			}),
 			resolve({
 				browser: true,
-				dedupe
+				dedupe,
 			}),
 			commonjs(),
 
 			legacy &&
 				babel({
-					extensions: [ '.js', '.mjs', '.html', '.svelte' ],
+					extensions: ['.js', '.mjs', '.html', '.svelte'],
 					runtimeHelpers: true,
-					exclude: [ 'node_modules/@babel/**' ],
+					exclude: ['node_modules/@babel/**'],
 					presets: [
 						[
 							'@babel/preset-env',
 							{
-								targets: '> 0.25%, not dead'
-							}
-						]
+								targets: '> 0.25%, not dead',
+							},
+						],
 					],
 					plugins: [
 						'@babel/plugin-syntax-dynamic-import',
 						[
 							'@babel/plugin-transform-runtime',
 							{
-								useESModules: true
-							}
-						]
-					]
+								useESModules: true,
+							},
+						],
+					],
 				}),
 
 			!dev &&
 				terser({
-					module: true
-				})
+					module: true,
+				}),
 		],
-		onwarn
+		onwarn,
 	},
 
 	server: {
@@ -113,27 +105,28 @@ export default {
 		plugins: [
 			replace({
 				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				'process.env.NODE_ENV': JSON.stringify(mode),
 			}),
 			svelte({
 				generate: 'ssr',
 				dev,
-				preprocess
+				preprocess,
 			}),
 			resolve({
-				dedupe
+				dedupe,
 			}),
 			commonjs(),
 			json(),
 			postcss({
 				plugins: postcssPlugins(!dev),
-				extract: path.resolve(__dirname, './static/global.css')
-			})
+				extract: path.resolve(__dirname, './static/global.css'),
+			}),
 		],
 		external: Object.keys(pkg.dependencies).concat(
-			require('module').builtinModules || Object.keys(process.binding('natives'))
+			require('module').builtinModules ||
+				Object.keys(process.binding('natives'))
 		),
-		onwarn
+		onwarn,
 	},
 
 	serviceworker: {
@@ -143,11 +136,11 @@ export default {
 			resolve(),
 			replace({
 				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				'process.env.NODE_ENV': JSON.stringify(mode),
 			}),
 			commonjs(),
-			!dev && terser()
+			!dev && terser(),
 		],
-		onwarn
-	}
+		onwarn,
+	},
 }
