87a83299f7e5241e361048510b818e2f907d27a6
Richard Van Camp <richard.vancamp@gofore.com>
2019-10-13 18:19:35 +0300

    chore (event-terminal): bump version

--- packages/wds-site-v4/src/components/Footer.svelte
+++ packages/wds-site-v4/src/components/Footer.svelte
@@ -12,6 +12,9 @@
 <div class="footer flex text-center justify-center align-middle">
 	<div class="flex flex-col justify-center mb-8">
 		<Mailto email="richard.vancamp@gmail.com">Contact us</Mailto>
-		<a href="https://github.com/tu4mo">Logo by <span class="text-term-brand-2">Tu4mo</span></a>
+		<a href="https://github.com/tu4mo">
+			Logo by
+			<span class="text-term-brand-1 font-bold">Tu4mo</span>
+		</a>
 	</div>
 </div>
