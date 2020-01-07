9d9d519ecd8819004f996c4a2f57752897bcc6e1
Leo Melin <leo.melin@mylab.fi>
2020-01-07 14:45:54 +0200

    Added mylab logo

--- packages/infra/ansible/deploy.sh
+++ packages/infra/ansible/deploy.sh
@@ -5,5 +5,5 @@ if [ "$2" == "quick" ]; then
   MODE=quick
 fi
 
-docker build . --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" -t wds-ansible-docker
+docker build . --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa_old)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa_old.pub)" -t wds-ansible-docker
 docker run --rm -v $(realpath ../../wds-event-tool):/opt/wds-event-tool -v $(realpath ../../wds-site-v4):/opt/wds-site-v4 -v $(realpath .):/opt/infra -it -w /opt/infra wds-ansible-docker ./entrypoint.sh $1 $2
