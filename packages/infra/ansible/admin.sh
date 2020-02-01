3e411fa68e3a85893a11328657854df73aa84663
Richard Van Camp <richard.vancamp@gmail.com>
2020-02-01 14:57:11 +0200

    Fix (Infra): ensure pip installs without errors

--- packages/infra/ansible/admin.sh
+++ packages/infra/ansible/admin.sh
@@ -3,4 +3,4 @@
 COMMAND=$2
 
 docker build . --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" -t wds-ansible-docker
-docker run --rm -v $(realpath ../../wds-event-tool):/opt/wds-event-tool -v $(realpath .):/opt/infra -it -w /opt/infra wds-ansible-docker ./admin-entrypoint.sh $1 $COMMAND
+docker run --rm -v $(npx realpath ../../wds-event-tool):/opt/wds-event-tool -v $(npx realpath .):/opt/infra -it -w /opt/infra wds-ansible-docker ./admin-entrypoint.sh $1 $COMMAND
--- packages/infra/ansible/deploy.sh
+++ packages/infra/ansible/deploy.sh
@@ -6,4 +6,4 @@ if [ "$2" == "quick" ]; then
 fi
 
 docker build . --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" -t wds-ansible-docker
-docker run --rm -v $(realpath ../../wds-event-tool):/opt/wds-event-tool -v $(realpath ../../wds-site-v4):/opt/wds-site-v4 -v $(realpath .):/opt/infra -it -w /opt/infra wds-ansible-docker ./entrypoint.sh $1 $2
+docker run --rm -v $(npx realpath ../../wds-event-tool):/opt/wds-event-tool -v $(npx realpath ../../wds-site-v4):/opt/wds-site-v4 -v $(npx realpath .):/opt/infra -it -w /opt/infra wds-ansible-docker ./entrypoint.sh $1 $2
