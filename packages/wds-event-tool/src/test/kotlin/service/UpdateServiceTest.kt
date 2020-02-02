7759ed6e32086ce56a9045b6f29c0c206f0af64a
Richard Van Camp <richard.vancamp@gmail.com>
2020-02-02 19:54:22 +0200

    Test (wds-event-tool): use fixed Clock for time tests

--- packages/wds-event-tool/src/test/kotlin/service/UpdateServiceTest.kt
+++ packages/wds-event-tool/src/test/kotlin/service/UpdateServiceTest.kt
@@ -1,29 +1,22 @@
 package service
 
-import arrow.core.toOption
 import io.kotlintest.Description
-import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import io.mockk.every
 import io.mockk.mockk
-import io.mockk.slot
 import meta.enums.EventStatus
 import meta.tables.Event.EVENT
 import meta.tables.records.EventRecord
-import org.webdevandsausages.events.dao.EventUpdates
-import org.webdevandsausages.events.dto.EventDto
 import org.webdevandsausages.events.dto.EventUpdateInDto
 import org.webdevandsausages.events.service.UpdateEventService
 import java.sql.Timestamp
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
-import java.time.temporal.TemporalAccessor
+import java.time.*
 
 class UpdateServiceTest : StringSpec() {
     lateinit var unit: UpdateEventService
+
+    private val now = 1550160535168L
+    private val fixedClock = Clock.fixed(Instant.ofEpochMilli(now), ZoneId.systemDefault())
 
     override fun beforeTest(description: Description) {
         unit = UpdateEventService(
@@ -38,11 +31,11 @@ class UpdateServiceTest : StringSpec() {
                 maxParticipants = 15,
                 sponsor = "testSponsor",
                 contact = "testContact",
-                date = Timestamp.valueOf(LocalDate.of(2019, 1, 1).atStartOfDay()),
+                date = Timestamp.valueOf(LocalDateTime.now(fixedClock)),
                 details = "testDetails",
                 location = "testLocation",
                 status = EventStatus.PLANNING,
-                registrationOpens = Timestamp.valueOf(LocalDateTime.of(2019, 2, 1, 0, 0)),
+                registrationOpens = Timestamp.valueOf(LocalDateTime.now(fixedClock)),
                 volume = 12,
                 sponsorLink = "testSponsorLink"
             )
@@ -52,11 +45,11 @@ class UpdateServiceTest : StringSpec() {
             record.get(EVENT.MAX_PARTICIPANTS).shouldBe(15)
             record.get(EVENT.SPONSOR).shouldBe("testSponsor")
             record.get(EVENT.CONTACT).shouldBe("testContact")
-            record.get(EVENT.DATE).shouldBe(OffsetDateTime.of(LocalDateTime.of(2019, 1, 1, 0, 0), ZoneOffset.ofHours(2)))
+            record.get(EVENT.DATE).shouldBe(OffsetDateTime.now(fixedClock))
             record.get(EVENT.DETAILS).shouldBe("testDetails")
             record.get(EVENT.LOCATION).shouldBe("testLocation")
             record.get(EVENT.STATUS).shouldBe(EventStatus.PLANNING)
-            record.get(EVENT.REGISTRATION_OPENS).shouldBe(Timestamp.valueOf(LocalDate.of(2019, 2, 1).atStartOfDay()))
+            record.get(EVENT.REGISTRATION_OPENS).shouldBe(Timestamp.valueOf(LocalDateTime.now(fixedClock)))
             record.get(EVENT.VOLUME).shouldBe(12)
             record.get(EVENT.SPONSOR_LINK).shouldBe("testSponsorLink")
 
