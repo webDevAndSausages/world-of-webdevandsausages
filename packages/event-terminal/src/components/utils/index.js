65c6c707e33877df1a507f54d524ae3c1417a41f
Richard Van Camp <richard.vancamp@gmail.com>
2020-02-03 06:10:17 +0200

    Chore: fix formating in termainal and add backend readme info

--- packages/event-terminal/src/components/utils/index.js
+++ packages/event-terminal/src/components/utils/index.js
@@ -1,5 +1,18 @@
-import { has, is, both, over, lensProp, propOr, compose, contains, lte, length, always, ifElse } from 'ramda'
-import { zonedTimeToUtc, utcToZonedTime, format } from 'date-fns-tz'
+import {
+	has,
+	is,
+	both,
+	over,
+	lensProp,
+	propOr,
+	compose,
+	contains,
+	lte,
+	length,
+	always,
+	ifElse,
+} from 'ramda'
+import {zonedTimeToUtc, utcToZonedTime, format} from 'date-fns-tz'
 import produce from 'immer'
 
 export const isEvent = both(is(Object), has('status'))
@@ -7,23 +20,32 @@ export const isEvent = both(is(Object), has('status'))
 const timeZone = 'Europe/Helsinki'
 const pattern = "MMMM do, yyyy, HH:mm 'GMT' XXX (z)"
 
-const toUtcDate = (date) => zonedTimeToUtc(date, timeZone)
-const toZonedDate = (date) => utcToZonedTime(new Date(date), timeZone)
-const formatWithZone = (zonedDate) => format(zonedDate, pattern, { timeZone })
+const toUtcDate = date => zonedTimeToUtc(date, timeZone)
+const toZonedDate = date => utcToZonedTime(new Date(date), timeZone)
+const formatWithZone = zonedDate => format(zonedDate, pattern, {timeZone})
 
-export const formatDate = compose(formatWithZone, toZonedDate, toUtcDate)
+export const formatDate = compose(
+	formatWithZone,
+	toZonedDate,
+	toUtcDate
+)
 
 export const formatEvent = over(lensProp('date'), formatDate)
 
-export const showForStatusOf = (...statuses) => compose((s) => contains(s, statuses), propOr({}, 'status'))
+export const showForStatusOf = (...statuses) =>
+	compose(
+		s => contains(s, statuses),
+		propOr({}, 'status')
+	)
 
 export const mapActions = (actions, state) =>
 	Object.keys(actions).reduce((acc, actionName) => {
-		acc[actionName] = (payload) => state.update((state) => produce(actions[actionName])(state, payload))
+		acc[actionName] = payload =>
+			state.update(state => produce(actions[actionName])(state, payload))
 		return acc
 	}, {})
 
-export const getPixelWidthOfText = (id) => (txt) => {
+export const getPixelWidthOfText = id => txt => {
 	let ruler = document.getElementById(id)
 	ruler.innerText = txt
 	return ruler.offsetWidth
@@ -31,39 +53,51 @@ export const getPixelWidthOfText = (id) => (txt) => {
 
 const commandsRegex = /^(r|register|x|cancel|c|check|h|help)$/
 
-export const isValidCmd = (value) => is(String, value) && commandsRegex.test(value.toLowerCase())
+export const isValidCmd = value =>
+	is(String, value) && commandsRegex.test(value.toLowerCase())
 
-export const normalizeCmd = (cmd) => cmd.trim().slice(0, 1).toLowerCase()
+export const normalizeCmd = cmd =>
+	cmd
+		.trim()
+		.slice(0, 1)
+		.toLowerCase()
 
 const terminalCommands = {
 	r: 'register',
 	x: 'cancel',
 	c: 'check',
-	h: 'help'
+	h: 'help',
 }
 
 const formCommands = {
 	s: 'submit',
 	x: 'cancel',
-	r: 'reset'
+	r: 'reset',
 }
 
-export const getFullTerminalCmd = (cmd) => {
+export const getFullTerminalCmd = cmd => {
 	const c = normalizeCmd(cmd)
 	return terminalCommands[c]
 }
 
-export const getFullFormCmd = (cmd) => {
+export const getFullFormCmd = cmd => {
 	const c = normalizeCmd(cmd)
 	return formCommands[c]
 }
 
 const emailRegex = /^.+@.+\..+$/i
-const isEmail = (value) => emailRegex.test(value)
-const minLen = (min) => compose(lte(min), length)
+const isEmail = value => emailRegex.test(value)
+const minLen = min =>
+	compose(
+		lte(min),
+		length
+	)
 const createValidator = (cond, msg) => ifElse(cond, always(null), always(msg))
 export const validateEmail = createValidator(isEmail, 'Invalid email')
 export const validateName = createValidator(minLen(1), 'A little longer please')
-export const isValidToken = createValidator(both(contains('-'), minLen(5)), 'Invalid token format')
+export const isValidToken = createValidator(
+	both(contains('-'), minLen(5)),
+	'Invalid token format'
+)
 
 export const isString = is(String)
--- packages/event-terminal/src/components/utils/request.js
+++ packages/event-terminal/src/components/utils/request.js
@@ -1,47 +1,46 @@
-
 import config from '../config'
 import ky from 'ky'
 import {Result} from '../models/Result'
 
 const createRequest = method => async (url, payload) => {
-  const requestConfig = {
-    method,
-    headers: config.headers,
-    throwHttpErrors: false
-  }
+	const requestConfig = {
+		method,
+		headers: config.headers,
+		throwHttpErrors: false,
+	}
 
-  if (payload) {
-    requestConfig.body = JSON.stringify(payload)
-  }
+	if (payload) {
+		requestConfig.body = JSON.stringify(payload)
+	}
 
-  const response = await ky(url, requestConfig)
+	const response = await ky(url, requestConfig)
 
-  let error = {
-    message: 'A mysterious error occurred on the server.',
-  }
+	let error = {
+		message: 'A mysterious error occurred on the server.',
+	}
 
-  if (!response.ok) {
-    try {
-      error = await response.json()
-    } catch (e) {
-      console.log('Failed to parse error response')
-    }
-    if (response.status === 404) {
-      error.message = 'Resource not found.'
-    }
-    return Result.Failure({
-      ...error,
-      status: response.status,
-    })
-  }
+	if (!response.ok) {
+		try {
+			error = await response.json()
+		} catch (e) {
+			console.log('Failed to parse error response')
+		}
+		if (response.status === 404) {
+			error.message = 'Resource not found.'
+		}
+		return Result.Failure({
+			...error,
+			status: response.status,
+		})
+	}
 
-  try {
-    const parsed = await response.json()
-    return Result.Ok(parsed)
-  } catch (e) {
-    console.log('Failed to parse response')
-    return Result.Failure({ status: 500, ...error })
-  }
+	try {
+		const parsed = await response.json()
+		return Result.Ok(parsed)
+	} catch (e) {
+		console.log('Failed to parse response')
+		return Result.Failure({status: 500, ...error})
+	}
 }
 
 export const apiGet = createRequest('GET')
