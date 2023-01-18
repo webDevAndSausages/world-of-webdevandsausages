a6e5b291858b798cca3b2439c70e71967013e1a0
Richard Van Camp <richard.van.camp@wolt.com>
2023-01-18 19:04:39 +0200

    Feat(firebase): remove unused functions

--- packages/firebase-functions/functions/src/services/db.ts
+++
@@ -1,7 +0,0 @@
-import admin from 'firebase-admin'
-
-const db = admin.firestore()
-export const participantsRef = db.collection('participants')
-export const eventsRef = db.collection('events')
-export const tokensRef = db.collection('api_tokens')
-export const adminsRef = db.collection('admins')
--- packages/firebase-functions/functions/src/services/mail.ts
+++
@@ -1,9 +0,0 @@
-const sgMail = require('@sendgrid/mail')
-import { tryP } from 'fluture'
-import { MailData } from '@sendgrid/helpers/classes/mail'
-import { config } from '..'
-
-export const sendMail = (msg: MailData) => {
-	sgMail.setApiKey(config.SENDGRID_KEY)
-	return tryP(() => sgMail.send(msg))
-}
